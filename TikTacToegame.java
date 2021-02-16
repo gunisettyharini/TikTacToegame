235682c7b68e41204e9121603da10769543546a8
Harini Gunisetty <gunisettyharini@gmail.com>
2021-02-16 10:36:19 +0530

    Ability to choose X or O

--- TikTacToegame.java
+++ TikTacToegame.java
@@ -1,12 +1,30 @@
 public class TikTacToegame {
-	public static void main(String[] args) {
-		char[] board = createboard();
-	}
-	private static char[] createboard() {
-		char[] board = new char[10];
-		for(int i=0; i < board.length ; i++) {
-			board[i] = ' ';
+	public static void printboard() {
+		char[] gameboard = new char[10];
+		for (int i=0; i<10; i++) {
+			gameboard[i]=' ';
+			System.out.println(gameboard[i]);
 		}
-		return board;
 	}
+	public static void player() {
+		Scanner user = new Scanner(System.in);
+		System.out.println("Enter a choice x or o");
+		char player = user.next().charAt(0);
+		char computer = ' ';
+		if(player == 'x' || player == 'X') {
+			computer = 'o';
+		}
+		else if (player == 'O' || player == 'o') {
+			computer = 'x';
+		}
+		else {
+			System.out.println("Invalid");
+		}
+		System.out.println("Player is " + player + "and Computer is " + computer);
+	}
+	public static void main(String args[]) {
+		System.out.println("Welcome to tictactoe game");
+		printboard();
+		player();
+ 	}
 }
